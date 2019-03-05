281c3ce0be35a07c664b6283df7fb8638ea2534a
Roger Jaffe <rogerjaffe@gmail.com>
2019-03-04 20:57:38 -0800

    Added 2010 question code

---
+++ q2010/q1/CookieOrder.java
@@ -0,0 +1,33 @@
+package q2010.q1;
+
+public class CookieOrder
+{
+  private String variety;
+  private int numBoxes;
+
+  /** Constructs a new CookieOrder object. */
+  public CookieOrder(String variety, int numBoxes)
+  {
+    this.variety = variety;
+    this.numBoxes = numBoxes;
+  }
+
+  /** @return the variety of cookie being ordered
+   */
+  public String getVariety()
+  {
+    return variety;
+  }
+
+  /** @return the number of boxes being ordered
+  */
+  public int getNumBoxes()
+  {
+    return numBoxes;
+  }
+
+  public String toString()
+  {
+    return variety + " " + numBoxes;
+  }
+}
---
+++ q2010/q2/APLine.java
@@ -0,0 +1,6 @@
+package q2010.q2;
+
+public class APLine
+{
+
+}
